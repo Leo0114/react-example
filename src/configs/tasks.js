f75ce199479653ec897b2a11570c211d9d19331f
Leo0114 <leonel0614@gmail.com>
2023-02-02 00:51:21 -0600

    Primer commit

---
+++ src/configs/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Mi primer tarea",
+  },
+  {
+    id: 2,
+    title: "Mi Segunda tarea",
+    description: "Mi Segunda tarea",
+  },
+  {
+    id: 3,
+    title: "Mi Tercer tarea",
+    description: "Mi Tercer tarea",
+  },
+];
